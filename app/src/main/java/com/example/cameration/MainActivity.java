be1181685ad3c47201195e6289cd50f214c40ae8
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-03-20 12:07:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cameration/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.cameration;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView foto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); //Esconde o banner
+        foto = findViewById(R.id.iV);
+        getWindow().setStatusBarColor(Color.rgb(29, 126, 204)); //Altera a cor da StatusBar
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+
+    public void tirarFoto(View v){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 &&  resultCode == RESULT_OK){
+            Bundle dado = data.getExtras();
+            Bitmap imagem = (Bitmap) dado.get("data");
+            foto.setImageBitmap(imagem);
+        }
+    }
+}
